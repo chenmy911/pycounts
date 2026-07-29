9e43863b7697882f00be9d976a69efcf15d1dd84
chenmy911 <coyboy911@gmail.com>
2026-07-29 20:13:47 +0800

    test: add additional tests for all modules

--- tests/test_pycounts.py
+++ tests/test_pycounts.py
@@ -1,11 +1,80 @@
 from pycounts.pycounts import count_words
 from collections import Counter
+from pycounts.plotting import plot_words
+from pycounts.datasets import get_flatland
+import matplotlib
+import pytest
 
-def test_count_words():
-    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1,
+@pytest.fixture
+def einstein_counts():
+    return Counter({'insanity': 1, 'is': 1, 'doing': 1,
                         'the': 1, 'same': 1, 'thing': 1,
                         'over': 2, 'and': 2, 'expecting': 1,
                         'different': 1, 'results': 1})
+
+
+def test_count_words(einstein_counts):
+    """Test word counting from a file."""
+    expected = einstein_counts
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
     print("Every thing works fine")
+
+
+def test_plot_words(einstein_counts):
+    """Test plotting of word counts."""
+    counts = einstein_counts
+    fig = plot_words(counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), \
+    "Wrong plot type"
+    assert len(fig.datavalues) == 10, \
+    "Incorrect number of bars plotted"
+
+@pytest.mark.parametrize(
+    "obj",
+    [ 3.141,
+      "test.txt",
+      ["list", "app", "words"]
+      ]
+)
+
+    
+def test_plot_words_error(obj):
+    """Check TypeError raised when Counter not used."""
+    with pytest.raises(TypeError):
+#        list_object = ["Pythons", "are", "non", "venomous"]
+        plot_words(obj)
+
+def test_integration():
+    """Test count_words() and plot_words() workflow."""
+    counts = count_words("tests/einstein.txt")
+    fig = plot_words(counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), \
+        "Wrong plot type"
+    assert len(fig.datavalues) == 10, \
+        "Incorrect number of bars plotted"
+    assert max(fig.datavalues) == 2, "Highest word count should be 2"
+
+def test_regression():
+    """Regression test for Flatland"""
+    top_word = count_words(get_flatland()).most_common(1)
+    assert top_word[0][0] == "the", "Most common word is not 'the'"
+    assert top_word[0][1] == 2245, "'the' count has changed"
+
+def is_even(n):
+    """Check if n is even."""
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+@pytest.mark.parametrize(
+    "n, result",
+    [
+        (2, True),
+        (3, False),
+        (4, True) # This last pair is purposefully wrong
+    ]
+)
+def test_is_even(n, result):
+    assert is_even(n) == result
